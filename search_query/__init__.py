c9df6c76ca58b33b12c250066404687caa778deb
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-03-12 12:11:38 +0100

    simplify import via __all__

--- search_query/__init__.py
+++ search_query/__init__.py
@@ -3,9 +3,11 @@
 __author__ = """Gerit Wagner"""
 __email__ = "gerit.wagner@hec.ca"
 
+from search_query.or_query import OrQuery
+from search_query.and_query import AndQuery
 from .__version__ import __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "OrQuery", "AndQuery"]
 
 # Instead of adding elements to __all__,
 # prefixing methods/variables with "__" is preferred.
