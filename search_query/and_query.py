de9923147b6ed9612e02a47880835129c6d47f0c
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2024-08-01 07:42:21 +0200

    allow search field as str

--- search_query/and_query.py
+++ search_query/and_query.py
@@ -16,20 +16,21 @@ class AndQuery(Query):
         self,
         children: typing.List[typing.Union[str, Query]],
         *,
-        search_field: SearchField,
+        search_field: typing.Union[SearchField, str],
         position: typing.Optional[tuple] = None,
     ) -> None:
         """init method
         search terms: strings which you want to include in the search query
         nested queries: queries whose roots are appended to the query
         search field: search field to which the query should be applied
-        options: Author Keywords, Abstract, Author, DOI, ISBN, Publisher or Title
         """
 
         super().__init__(
             value=Operators.AND,
             operator=True,
             children=children,
-            search_field=search_field,
+            search_field=search_field
+            if isinstance(search_field, SearchField)
+            else SearchField(search_field),
             position=position,
         )
