8a2be23d60ab6d6d21a1aae0e23256364740ffbf
Gerit Wagner <geritwagner@users.noreply.github.com>
2025-03-11 14:38:23 +0100

    Update __init__.py

--- search_query/__init__.py
+++ search_query/__init__.py
@@ -3,11 +3,12 @@
 __author__ = """Gerit Wagner"""
 __email__ = "gerit.wagner@hec.ca"
 
+from search_query.query import Query
 from search_query.or_query import OrQuery
 from search_query.and_query import AndQuery
 from .__version__ import __version__
 
-__all__ = ["__version__", "OrQuery", "AndQuery"]
+__all__ = ["__version__", "Query", "OrQuery", "AndQuery"]
 
 # Instead of adding elements to __all__,
 # prefixing methods/variables with "__" is preferred.
