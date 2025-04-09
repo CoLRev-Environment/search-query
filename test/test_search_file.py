e9e6dd3105fffb73710800f56b0844efbef4968d
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-04-09 10:12:21 +0200

    add load_search_file

--- test/test_search_file.py
+++ test/test_search_file.py
@@ -2,14 +2,12 @@
 """Tests for SearchFile parser."""
 from __future__ import annotations
 
-from search_query.search_file import SearchFile
+from search_query.search_file import load_search_file
 
 
 def test_search_history_file_parser() -> None:
     """Test SearchFile parser."""
 
-    file_path = "test/search_history_file_1.json"
-
-    result = SearchFile(file_path)
+    result = load_search_file("test/search_history_file_1.json")
 
     assert hasattr(result, "parsed")
