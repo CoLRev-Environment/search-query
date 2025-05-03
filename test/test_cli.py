617a87df915eb7e4d6e0e4120c7eae488ca09ce0
Gerit Wagner <gerit.wagner@uni-bamberg.de>
2025-05-03 10:06:09 +0200

    update cli test

--- test/test_cli.py
+++ test/test_cli.py
@@ -3,36 +3,6 @@
 import subprocess
 from pathlib import Path
 
-from search_query import load_search_file
-
-
-def test_translate_cli() -> None:
-    test_data_dir = Path(__file__).parent
-    input_file = test_data_dir / "search_history_file_1.json"
-    output_file = test_data_dir / "output.json"
-
-    result = subprocess.run(
-        [
-            "search-query-translate",
-            "--from=wos",
-            f"--input={input_file}",
-            "--to=ebscohost",
-            f"--output={output_file}",
-        ],
-        capture_output=True,
-        text=True,
-    )
-    print(result.stdout)
-    print(result.stderr)
-    assert result.returncode == 0
-    assert "Converting from wos to ebscohost" in result.stdout
-    assert "Writing converted query to" in result.stdout
-    assert output_file.exists()
-
-    result_file = load_search_file(output_file)
-    assert result_file.search_string == "TP (quantum AND dot AND spin)"
-
-
 def test_linter_cli() -> None:
     test_data_dir = Path(__file__).parent
     input_file = test_data_dir / "search_history_file_2_linter.json"
